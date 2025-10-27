979678b256ffbbf60b79e7d907adea47b1524b9a
Gilles Peskine <Gilles.Peskine@arm.com>
2025-10-27 18:01:36 +0100

    Include common.h before system headers
    In library source files, the order of things should be:
    
    1. Define macros that affect the behavior of system headers, such as
       `_POSIX_C_SOURCE` and `_GNU_SOURCE`.
    2. Include the library's common header: `tf_psa_crypto_common.h`.
       It takes care of many things, including defining the library
       configuration, granting access to private fields in structures, and
       activating platform-specific hacks.
    3. Possibly a few header inclusions and macro definitions.
    4. Guard everything else by `#if defined(MBEDTLS_XXX_C)` or some such.
    
    Enforce this order in files that previously did things they shouldn't have
    before including `tf_psa_crypto_common.h`. To locate the potentially
    problematic files:
    
    ```
    grep -m1 '^#' core/*.c drivers/builtin/src/*.c | grep -v tf_psa_crypto_common
    ```
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- drivers/builtin/src/pk.c
+++ drivers/builtin/src/pk.c
@@ -5,12 +5,6 @@
  *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
  */
 
-/*
- * This file is private and for internal
- * use only.
- */
-#define MBEDTLS_DECLARE_PRIVATE_IDENTIFIERS
-
 #include "tf_psa_crypto_common.h"
 
 #if defined(MBEDTLS_PK_C)
