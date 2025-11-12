2b2178e325db87e6dc0bed4dfc1f50a586695cd4
Ben Taylor <ben.taylor@linaro.org>
2025-11-12 10:28:14 +0000

    replace rsa_pss_expected_salt_len in the call to mbedtls_rsa_rsassa_pss_sign_ext with MBEDTLS_RSA_SALT_LEN_ANY
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- drivers/builtin/src/psa_crypto_rsa.c
+++ drivers/builtin/src/psa_crypto_rsa.c
@@ -313,29 +313,6 @@ static psa_status_t psa_rsa_decode_md_type(psa_algorithm_t alg,
     return PSA_SUCCESS;
 }
 
-#if defined(MBEDTLS_PSA_BUILTIN_ALG_RSA_PSS)
-static int rsa_pss_expected_salt_len(psa_algorithm_t alg,
-                                     const mbedtls_rsa_context *rsa,
-                                     size_t hash_length)
-{
-    if (PSA_ALG_IS_RSA_PSS_ANY_SALT(alg)) {
-        return MBEDTLS_RSA_SALT_LEN_ANY;
-    }
-    /* Otherwise: standard salt length, i.e. largest possible salt length
-     * up to the hash length. */
-    int klen = (int) mbedtls_rsa_get_len(rsa);   // known to fit
-    int hlen = (int) hash_length; // known to fit
-    int room = klen - 2 - hlen;
-    if (room < 0) {
-        return 0;  // there is no valid signature in this case anyway
-    } else if (room > hlen) {
-        return hlen;
-    } else {
-        return room;
-    }
-}
-#endif /* MBEDTLS_PSA_BUILTIN_ALG_RSA_PSS */
-
 psa_status_t mbedtls_psa_rsa_sign_hash(
     const psa_key_attributes_t *attributes,
     const uint8_t *key_buffer, size_t key_buffer_size,
@@ -391,8 +368,7 @@ psa_status_t mbedtls_psa_rsa_sign_hash(
                                                   MBEDTLS_MD_NONE,
                                                   (unsigned int) hash_length,
                                                   hash,
-                                                  rsa_pss_expected_salt_len(MBEDTLS_MD_NONE, rsa,
-                                                                            hash_length),
+                                                  MBEDTLS_RSA_SALT_LEN_ANY,
                                                   signature);
         }
     } else
@@ -414,6 +390,28 @@ exit:
     return status;
 }
 
+#if defined(MBEDTLS_PSA_BUILTIN_ALG_RSA_PSS)
+static int rsa_pss_expected_salt_len(psa_algorithm_t alg,
+                                     const mbedtls_rsa_context *rsa,
+                                     size_t hash_length)
+{
+    if (PSA_ALG_IS_RSA_PSS_ANY_SALT(alg)) {
+        return MBEDTLS_RSA_SALT_LEN_ANY;
+    }
+    /* Otherwise: standard salt length, i.e. largest possible salt length
+     * up to the hash length. */
+    int klen = (int) mbedtls_rsa_get_len(rsa);   // known to fit
+    int hlen = (int) hash_length; // known to fit
+    int room = klen - 2 - hlen;
+    if (room < 0) {
+        return 0;  // there is no valid signature in this case anyway
+    } else if (room > hlen) {
+        return hlen;
+    } else {
+        return room;
+    }
+}
+#endif /* MBEDTLS_PSA_BUILTIN_ALG_RSA_PSS */
 
 psa_status_t mbedtls_psa_rsa_verify_hash(
     const psa_key_attributes_t *attributes,
