joseguzman1337/TF-PSA-Crypto

commit 979678b256ffbbf60b79e7d907adea47b1524b9a

Include common.h before system headers
In library source files, the order of things should be:

1. Define macros that affect the behavior of system headers, such as
   `_POSIX_C_SOURCE` and `_GNU_SOURCE`.
2. Include the library's common header: `tf_psa_crypto_common.h`.
   It takes care of many things, including defining the library
   configuration, granting access to private fields in structures, and
   activating platform-specific hacks.
3. Possibly a few header inclusions and macro definitions.
4. Guard everything else by `#if defined(MBEDTLS_XXX_C)` or some such.

Enforce this order in files that previously did things they shouldn't have
before including `tf_psa_crypto_common.h`. To locate the potentially
problematic files:

```
grep -m1 '^#' core/*.c drivers/builtin/src/*.c | grep -v tf_psa_crypto_common
```

Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

drivers/builtin/src/aesce.c
@@ -5,6 +5,10 @@
  *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
  */
 
+#include "tf_psa_crypto_common.h"
+
+#if defined(MBEDTLS_AESCE_C)
+
 #if defined(__clang__) &&  (__clang_major__ >= 4)
 
 /* Ideally, we would simply use MBEDTLS_ARCH_IS_ARMV8_A in the following #if,
@@ -39,9 +43,6 @@
 #endif /* defined(__clang__) &&  (__clang_major__ >= 4) */
 
 #include <string.h>
-#include "tf_psa_crypto_common.h"
-
-#if defined(MBEDTLS_AESCE_C)
 
 #include "aesce.h"
 

drivers/builtin/src/base64.c
@@ -5,8 +5,6 @@
  *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
  */
 
-#include <limits.h>
-
 #include "tf_psa_crypto_common.h"
 
 #if defined(MBEDTLS_BASE64_C)
@@ -16,6 +14,7 @@
 #include "constant_time_internal.h"
 #include "mbedtls/private/error_common.h"
 
+#include <limits.h>
 #include <stdint.h>
 
 #if defined(MBEDTLS_SELF_TEST)

drivers/builtin/src/constant_time.c
@@ -10,16 +10,15 @@
  * might be translated to branches by some compilers on some platforms.
  */
 
-#include <stdint.h>
-#include <limits.h>
-
 #include "tf_psa_crypto_common.h"
 #include "constant_time_internal.h"
 #include "mbedtls/constant_time.h"
 #include "mbedtls/private/error_common.h"
 #include "mbedtls/platform_util.h"
 
+#include <limits.h>
 #include <string.h>
+#include <stdint.h>
 
 #if !defined(MBEDTLS_CT_ASM)
 /*

drivers/builtin/src/pk.c
@@ -5,12 +5,6 @@
  *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
  */
 
-/*
- * This file is private and for internal
- * use only.
- */
-#define MBEDTLS_DECLARE_PRIVATE_IDENTIFIERS
-
 #include "tf_psa_crypto_common.h"
 
 #if defined(MBEDTLS_PK_C)

drivers/builtin/src/sha256.c
@@ -10,6 +10,15 @@
  *  http://csrc.nist.gov/publications/fips/fips180-2/fips180-2.pdf
  */
 
+/* Ensure that SIG_SETMASK is defined when -std=c99 is used. */
+#if !defined(_GNU_SOURCE)
+#define _GNU_SOURCE
+#endif
+
+#include "tf_psa_crypto_common.h"
+
+#if defined(MBEDTLS_SHA256_C) || defined(MBEDTLS_SHA224_C)
+
 #if defined(__clang__) &&  (__clang_major__ >= 4)
 
 /* Ideally, we would simply use MBEDTLS_ARCH_IS_ARMV8_A in the following #if,
@@ -42,15 +51,6 @@
 #endif
 
 #endif /* defined(__clang__) &&  (__clang_major__ >= 4) */
-
-/* Ensure that SIG_SETMASK is defined when -std=c99 is used. */
-#if !defined(_GNU_SOURCE)
-#define _GNU_SOURCE
-#endif
-
-#include "tf_psa_crypto_common.h"
-
-#if defined(MBEDTLS_SHA256_C) || defined(MBEDTLS_SHA224_C)
 
 #include "mbedtls/private/sha256.h"
 #include "mbedtls/platform_util.h"

drivers/builtin/src/sha512.c
@@ -10,6 +10,10 @@
  *  http://csrc.nist.gov/publications/fips/fips180-2/fips180-2.pdf
  */
 
+#include "tf_psa_crypto_common.h"
+
+#if defined(MBEDTLS_SHA512_C) || defined(MBEDTLS_SHA384_C)
+
 #if defined(__aarch64__) && !defined(__ARM_FEATURE_SHA512) && \
     defined(__clang__) && __clang_major__ >= 7
 /* TODO: Re-consider above after https://reviews.llvm.org/D131064 merged.
@@ -25,10 +29,6 @@
 #define __ARM_FEATURE_SHA512 1
 #define MBEDTLS_ENABLE_ARM_SHA3_EXTENSIONS_COMPILER_FLAG
 #endif
-
-#include "tf_psa_crypto_common.h"
-
-#if defined(MBEDTLS_SHA512_C) || defined(MBEDTLS_SHA384_C)
 
 #include "mbedtls/private/sha512.h"
 #include "mbedtls/platform_util.h"

commit 71b3eca2bf44ea3043878010eae39b987070d892

AESCE: fix the build with Clang <<16
As indicated in a comment, with older Clang, we need to define
`__ARM_FEATURE_CRYPTO` before we include `arm_neon.h`, which is done via
`tf_psa_crypto_common.h`. 979678b256ffbbf60b79e7d907adea47b1524b9a
accidentally broke this. Fix it, while taking care not to include another
header before `tf_psa_crypto_common.h`.

Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

drivers/builtin/src/aesce.c
@@ -5,10 +5,6 @@
  *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
  */
 
-#include "tf_psa_crypto_common.h"
-
-#if defined(MBEDTLS_AESCE_C)
-
 #if defined(__clang__) &&  (__clang_major__ >= 4)
 
 /* Ideally, we would simply use MBEDTLS_ARCH_IS_ARMV8_A in the following #if,
@@ -41,6 +37,10 @@
 #endif
 
 #endif /* defined(__clang__) &&  (__clang_major__ >= 4) */
+
+#include "tf_psa_crypto_common.h"
+
+#if defined(MBEDTLS_AESCE_C)
 
 #include <string.h>
 